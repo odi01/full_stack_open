3a3246f92b71ed1087bcf9960d51bad7ea54d4d7
Ido <amsalem1999@gmail.com>
2023-05-17 19:51:15 +0300

    added the customer feedback exercise, untill 1.9

---
+++ fullstack_open_exercises/part1/customer_feedback/src/App.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
+
+const Statistics = (props) => (
+  <p>
+    {props.text} {props.value}
+  </p>
+);
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const total = good + neutral + bad;
+
+  if (total != 0) {
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <br />
+        <Button handleClick={() => setGood(good + 1)} text="good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="bad" />
+
+        <h1>statistics</h1>
+        <Statistics text="good" value={good} />
+        <Statistics text="neutral" value={neutral} />
+        <Statistics text="bad" value={bad} />
+        <Statistics text="all" value={total} />
+        <Statistics text="average" value={total / 3} />
+        <Statistics text="positive" value={(good / total) * 100} />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <br />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+
+      <h1>statistics</h1>
+      <p>No feedback given</p>
+    </div>
+  );
+};
+
+export default App;
