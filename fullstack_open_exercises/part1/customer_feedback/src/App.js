1daf9671c8725a4dd775ea49eecd42051ba3e4eb
Ido <amsalem1999@gmail.com>
2023-06-25 20:23:30 +0300

    ex 1.13*: anecdotes step2 completed

--- fullstack_open_exercises/part1/customer_feedback/src/App.js
+++ fullstack_open_exercises/part1/customer_feedback/src/App.js
@@ -5,41 +5,85 @@ const Button = (props) => (
 );
 
 const Statistics = (props) => (
-  <p>
-    {props.text} {props.value}
-  </p>
+  <tr>
+    <td>
+      {props.text} {props.value}
+    </td>
+  </tr>
 );
 
 const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   const total = good + neutral + bad;
 
+  const handleVote = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  };
+
+  const handleNextAnecdote = () => {
+    const randomAnecdoteIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomAnecdoteIndex);
+  };
+
+  console.log(votes)
+
   return (
     <div>
       <h1>give feedback</h1>
+      <br />
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <Button handleClick={handleVote} text="vote" />
+      <Button handleClick={handleNextAnecdote} text="next anecdote" />
+      <br />
       <br />
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
 
       <h1>statistics</h1>
-      {total !== 0 ? (
-        <>
-          <Statistics text="good" value={good} />
-          <Statistics text="neutral" value={neutral} />
-          <Statistics text="bad" value={bad} />
-          <Statistics text="all" value={total} />
-          <Statistics text="average" value={total / 3} />
-          <Statistics text="positive" value={(good / total) * 100} />
-        </>
-      ) : (
-        <p>No feedback given</p>
-      )}
+      <table>
+        <tbody>
+          {total !== 0 ? (
+            <>
+              <Statistics text="good" value={good} />
+              <Statistics text="neutral" value={neutral} />
+              <Statistics text="bad" value={bad} />
+              <Statistics text="all" value={total} />
+              <Statistics text="average" value={total / 3} />
+              <Statistics text="positive" value={(good / total) * 100 + "%"} />
+              {anecdotes.map((anecdote, index) => (
+                <Statistics key={index} text={anecdote} value={votes[index]} />
+              ))}
+            </>
+          ) : (
+            <tr>
+              <td>No feedback given</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
 
 export default App;
+
+///1.13*: anecdotes step2
