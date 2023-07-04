367c67d50e3a087fe3b7b0ebcc693838b489d285
Ido <amsalem1999@gmail.com>
2023-07-04 21:35:46 +0300

    2.9*: The Phonebook Step4

--- fullstack_open_exercises/part2/phonebook/src/App.js
+++ fullstack_open_exercises/part2/phonebook/src/App.js
@@ -1,11 +1,55 @@
 import { useState } from "react";
 
+const Form = ({
+  newName,
+  newNumber,
+  handleNameChange,
+  handleNumberChange,
+  addPerson,
+}) => {
+  return (
+    <form>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+        <br />
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit" onClick={addPerson}>
+          add
+        </button>
+      </div>
+    </form>
+  );
+};
+
+const Phonebook = ({ phonebook }) => {
+  return (
+    <table>
+      <tbody>
+        {phonebook.map((contact) => (
+          <tr key={contact.id}>
+            <td>
+              {contact.name} {contact.number}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
 const App = () => {
-  const [phonebook, setPhonebook] = useState([
-    { name: "Arto Hellas", number: "054-5415482" },
-  ]);
+  const initialPhonebook = [
+    { id: 1, name: "Arto Hellas", number: "040-123456" },
+    { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
+    { id: 3, name: "Dan Abramov", number: "12-43-234345" },
+    { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" },
+  ];
+  const [phonebook, setPhonebook] = useState(initialPhonebook);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterKeyword, setFilterKeyword] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -34,34 +78,47 @@ const App = () => {
     }
   };
 
+  const filterKeywordMatch = (keyword) => {
+    return initialPhonebook.filter((contact) =>
+      contact.name.toLowerCase().includes(keyword.toLowerCase())
+    );
+  };
+
+  const handleKeyword = (event) => {
+    const keyword = event.target.value;
+    setFilterKeyword(keyword);
+    if (keyword === "") {
+      setPhonebook(initialPhonebook);
+    } else {
+      const matches = filterKeywordMatch(keyword);
+
+      if (matches.length === 0) {
+        setPhonebook([]);
+      } else {
+        setPhonebook(matches);
+      }
+    }
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
-          <br />
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit" onClick={addPerson}>
-            add
-          </button>
+          filter shown with:{" "}
+          <input value={filterKeyword} onChange={handleKeyword} />
         </div>
       </form>
-
+      <h2>Add New</h2>
+      <Form
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        addPerson={addPerson}
+      />
       <h2>Numbers</h2>
-      <table>
-        <tbody>
-          {phonebook.map((contact, index) => (
-            <tr key={index}>
-              <td>
-                {contact.name} {contact.number}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <Phonebook phonebook={phonebook} />
     </div>
   );
 };
