2b1960872f285512cd85bfadfb6b9c7a2176d2eb
Ido <amsalem1999@gmail.com>
2023-12-25 22:17:02 +0200

    ex4.18-4.19

--- fullstack_open_exercises/part4/app.js
+++ fullstack_open_exercises/part4/app.js
@@ -8,6 +8,7 @@ require("express-async-errors");
 
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 const middleware = require("./utils/middleware");
 
@@ -31,6 +32,7 @@ app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 // app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler);
--- fullstack_open_exercises/part4/controllers/blogs.js
+++ fullstack_open_exercises/part4/controllers/blogs.js
@@ -1,62 +1,84 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (request) => {
+	const authorization = request.get("authorization");
+	if (authorization && authorization.startsWith("Bearer ")) {
+		return authorization.replace("Bearer ", "");
+	}
+	return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
-  response.json(blogs);
+	const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
+	response.json(blogs);
 });
 
-blogsRouter.post("/", async (request, response, next) => {
-  // if ("likes" in request.body === false) {
-  //   console.log("Adding likes property equal to: 0");
-  //   request.body["likes"] = 0;
-  // }
-  try {
-    const blog = new Blog(request.body);
-    const savedBlog = await blog.save();
-    response.status(201).json(savedBlog);
-  } catch (error) {
-    console.error("Validation error:", error.message);
-    response.status(400).json({ error: error.message });
-  }
+blogsRouter.post("/", async (request, response) => {
+	// if ("likes" in request.body === false) {
+	//   console.log("Adding likes property equal to: 0");
+	//   request.body["likes"] = 0;
+	// }
+	const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+	if (!decodedToken.id) {
+		return response.status(401).json({ error: "token invalid" });
+	}
+	try {
+		const user = await User.findById(decodedToken.id);
+
+		request.body.user = user._id;
+		const blog = new Blog(request.body);
+		const savedBlog = await blog.save();
+
+		// Save the blog id to the user object
+		user.blogs = user.blogs.concat(savedBlog._id);
+		await user.save();
+
+		response.status(201).json(savedBlog);
+	} catch (error) {
+		console.error("Validation error:", error.message);
+		response.status(400).json({ error: error.message });
+	}
 });
 
 blogsRouter.get("/:id", async (request, response, next) => {
-  const blog = await Blog.findById(request.params.id);
-  if (blog) {
-    response.json(blog);
-  } else {
-    response.status(404).end();
-  }
+	const blog = await Blog.findById(request.params.id);
+	if (blog) {
+		response.json(blog);
+	} else {
+		response.status(404).end();
+	}
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
-  try {
-    await Blog.findByIdAndDelete(request.params.id);
-    response.status(204).end();
-  } catch (error) {
-    console.error("Failed to delete:", error.message);
-    response.status(400).json({ error: error.message });
-  }
+	try {
+		await Blog.findByIdAndDelete(request.params.id);
+		response.status(204).end();
+	} catch (error) {
+		console.error("Failed to delete:", error.message);
+		response.status(400).json({ error: error.message });
+	}
 });
 
 blogsRouter.put("/:id", async (request, response) => {
-  const blog = {
-    likes: request.body.likes,
-  };
+	const blog = {
+		likes: request.body.likes,
+	};
 
-  try {
-    updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
-      new: true,
-    });
-    if (updatedBlog === null || updatedBlog === undefined) {
-      throw new Error("Blog not exists");
-    }
-    response.status(201).json(updatedBlog);
-  } catch (error) {
-    console.error("Failed to update blog:", error.message);
-    response.status(400).json({ error: error.message });
-  }
+	try {
+		updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+			new: true,
+		});
+		if (updatedBlog === null || updatedBlog === undefined) {
+			throw new Error("Blog not exists");
+		}
+		response.status(201).json(updatedBlog);
+	} catch (error) {
+		console.error("Failed to update blog:", error.message);
+		response.status(400).json({ error: error.message });
+	}
 });
 
 module.exports = blogsRouter;
