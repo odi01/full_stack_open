c61713bd6e520bd119c3d7367bacb39ec60af4f0
Ido <amsalem1999@gmail.com>
2023-12-25 23:00:18 +0200

    4.21

--- fullstack_open_exercises/part4/controllers/blogs.js
+++ fullstack_open_exercises/part4/controllers/blogs.js
@@ -45,7 +45,17 @@ blogsRouter.get("/:id", async (request, response, next) => {
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
+	const decodedToken = jwt.verify(request.token, process.env.SECRET);
+	if (!decodedToken.id) {
+		return response.status(401).json({ error: "token invalid" });
+	}
+
 	try {
+		const blog = await Blog.findById(request.params.id);
+		if (blog.user.toString() !== decodedToken.id.toString()) {
+			return response.status(403).json({ error: "no permissions" });
+		}
+
 		await Blog.findByIdAndDelete(request.params.id);
 		response.status(204).end();
 	} catch (error) {
